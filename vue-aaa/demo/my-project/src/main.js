a1f27726bf110eea83cebde0fea67cab09578738
zoe-jqq <834245916@qq.com>
2019-04-24 21:31:32 +0800

    4.24

--- vue-aaa/demo/my-project/src/main.js
+++ vue-aaa/demo/my-project/src/main.js
@@ -3,9 +3,20 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import './lib/mui/css/mui.min.css'
+// 导入扩展图标样式
+import './lib/mui/css/icons-extra.css'
 
-Vue.config.productionTip = false
-
+import VueResource from 'vue-resource'
+// 2.2 安装 vue-resource
+Vue.use(VueResource)
+// 按需导入 Mint-UI 中的组件   
+import { Header, Swipe, SwipeItem } from 'mint-ui'
+Vue.component(Header.name, Header)
+Vue.component(Swipe.name, Swipe)
+Vue.component(SwipeItem.name, SwipeItem)
+// Vue.config.productionTip = false
+	// setHeader('access-Control-Allow-Origin:*');
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
